4536f00fdb13fa1f2bfde84b6e2367f6dd8335bf
AdrianoMera <adrianoklopes13@gmail.com>
2024-08-29 09:46:26 -0300

    limpando

--- aula01.js
+++
@@ -1,4 +0,0 @@
-//console.log("Esta linha está comentada")
-/*console.log("Para comentar mais que uma linha")
-console.log("outra linha comentada")*/
-console.log("Alô Mundo!");
--- aula02.js
+++
@@ -1,4 +0,0 @@
-let nome="Adriano"
-console.log(nome)
-console.log('Nome: ' + nome + 'é lindo !')
-console.log(nome)
