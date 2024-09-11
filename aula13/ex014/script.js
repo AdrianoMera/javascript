7558b5edeb5655fab02533387b6da2b97fd3647a
AdrianoMera <adrianoklopes13@gmail.com>
2024-09-11 20:52:58 -0300

    tela de horario

---
+++ aula13/ex014/script.js
@@ -0,0 +1,11 @@
+function carregar() {
+  var msg = document.getElementById("msg");
+  var foto = document.getElementById("foto");
+  var data = new Date()
+  var hora = data.getHours()
+  msg.innerHTML = `Agora são ${hora} horas.`
+
+  if (hora >= 0 && hora < 12){
+    // BOM DIA
+  }ELSE
+}
